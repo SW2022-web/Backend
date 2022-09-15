9b2ce1ab65fc773374b8900bb891f081deadeee4
SeungJooKim <seoungjoo01@gmail.com>
2022-09-15 17:04:14 +0900

    Initial Setting

---
+++ mysite/chahynsoo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChahynsooConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "chahynsoo"
---
+++ mysite/dvp_tae/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DvpTaeConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dvp_tae"
